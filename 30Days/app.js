a60be9e2d7917343de016bb04a3865616956cbfa
acgrp103 <acgrp103@gmail.com>
2025-08-20 18:54:32 +0900

    JS-Start

---
+++ 30Days/app.js
@@ -0,0 +1,8 @@
+let num1 = 32;
+let userName = "Hi, I'm Jin";
+let hobbies = ['Sports', 'Game', 'Reading'];
+let job = { title: 'Developer', place: 'South Korea', salary: 50000 };
+
+alert(hobbies[0]);
+alert(hobbies[1]);
+alert(hobbies[2]);

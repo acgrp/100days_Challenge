10d3a4198e8c5b8696ea87b76c48ef15ef00b824
acgrp103 <acgrp103@gmail.com>
2025-08-21 20:07:33 +0900

    function

--- 30Days/app.js
+++ 30Days/app.js
@@ -1,8 +1,28 @@
-let num1 = 32;
-let userName = "Hi, I'm Jin";
-let hobbies = ['Sports', 'Game', 'Reading'];
-let job = { title: 'Developer', place: 'South Korea', salary: 50000 };
+let age= 32;
+let userName = 'Jin';
+let hobbies = [
+    'Sports', 
+    'Game', 
+    'Reading'
+];
+let job = { 
+    title: 'Developer', 
+    place: 'South Korea', 
+    salary: 50000 
+};
 
-alert(hobbies[0]);
-alert(hobbies[1]);
-alert(hobbies[2]);
+let totaladultYears;
+
+function calculateAdultYears(userAge) {
+    let Result;
+    result = userAge - 18;
+    return result;
+}
+
+totaladultYears = calculateAdultYears(age);
+alert(totaladultYears);
+
+age = 45;
+totaladultYears = calculateAdultYears(age);
+
+alert(totaladultYears);

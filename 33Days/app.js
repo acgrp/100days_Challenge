85a916b82d0a042c7117cdf9078b1c0a84a579eb
acgrp103 <acgrp103@gmail.com>
2025-08-27 18:01:55 +0900

    34Days

--- 33Days/app.js
+++ 33Days/app.js
@@ -1,6 +1,20 @@
-document.body.children[1].children[0].href = 'http://google.com';
+// document.body.children[1].children[0].href = 'http://google.com';
 
 //  comsole.dir(document);
 // alert();
 // window.alert();
 
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'http://google.com';
+
+anchorElement = document.querySelector('p a');//All : 모든, 없으면 p a의 첫번째에
+anchorElement.href = 'http://academind.com';
+
+
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href = 'http://google.com';
+newAnchorElement.textContent = 'This leads to Google!';
+
+let firstParagraph = document.querySelector('p');
+
+firstParagraph.append(newAnchorElement);

eb9f2985ef356d89a912726b3599e1f6ad908c19
acgrp103 <acgrp103@gmail.com>
2025-09-01 17:27:24 +0900

    36DAYS+JS-MEMO

---
+++ 33Days/demo.js
@@ -0,0 +1,16 @@
+let productNameInputElement = document.getElementById('product-name');
+let remainingCharsElement = document.getElementById('remaining-chars');
+
+let maxAllowedChars = productNameInputElement.maxLength;
+
+
+function updateRemainingCharacters() {
+    let enteredText = event.target.value;
+    let enteredTextLength = enteredText.length;
+
+    let remainingCharacters = maxAllowedChars- enteredTextLength
+    
+    remainingCharsElement.textContent = remainingCharacters;
+}
+
+productNameInputElement.addEventListener('input', updateRemainingCharacters);

5bba968f582775e13509aba459eadd4feb5879e2
acgrp103 <acgrp103@gmail.com>
2025-09-16 13:34:29 +0900

    48Days-expressJS

--- 47Days/app.js
+++ 47Days/app.js
@@ -1,24 +1,22 @@
-const http = require('http');
+const express = require('express'); //package에 express 필요
 
-function handleRequest(request, response) {
+const app = express();
 
-    if (request.url ==='/currenttime') {
-        response.statusCode = 200; //success , 404 - error, 401 - error, 500 - error
-        response.end('<h1>' + new Date().toISOString() + '</h1>');        
-    } else if (request.url === '/') {
-        response.statusCode = 200; //success , 404 - error, 401 - error, 500 - error
-        response.end('<h1>Hello World!</h1>');
-    }
-    //localhost:3000/
-}
+app.use(express.urlencoded({extende: false}));//get,post와 달리 요청종류 신경안씀, urlencoded : 바디 파서를 설정하는 메서드
 
+app.get('/currenttime', function(request, response) {//request : 들어오는 요청에 대한 추가 정보 제공, response : 응답을 준비하는 기능 제공, next : 함수 내부에서 사용가능한 함수
+    response.send('<h1>' + new Date().toISOString() + '</h1>');  //send : 데이터로 보낼 매개변수 값으로 전달 가능(end와 비슷)
+}); //localhost:3000/currenttime
 
+app.get('/', function(request, response){
+    response.send('<form action="store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>');
+}); //localhost:3000/
 
-const server = http.createServer(handleRequest);
+app.post('/store-user', function(request, response){//위에서 store-user로 지정, post인이유도 동일(get으로 변경 가능)
+    const userName = request.body.username;
+    console.log(userName);
+    response.send('<h1>Username stored!</h1>');
+});
 
-server.listen(3000);
+app.listen(3000);
 
-
-
-// 주소입력 => 서버에 요청 보내서 사이트를 반환
-// 

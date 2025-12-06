928882b012818d20e7ca97f754bb602017b367c9
acgrp <acgrp103@naver.com>
2025-12-06 17:52:21 +0900

    85Days

--- 78Days/controllers/admin.controller.js
+++ 78Days/controllers/admin.controller.js
@@ -1,12 +1,35 @@
-function getProducts(req, res) {
-    res.render('admin/products/all-product');
+const Product = require('../models/product.model');
+
+async function getProducts(req, res) {
+    try {
+        const products = await Product.findAll();
+        res.render('admin/products/all-product', {products: products });
+    } catch (error) {
+        next(error);
+        return;
+    }
 }
 
 function getNewProduct(req, res) {
     res.render('admin/products/new-product');
 }
 
-function createNewProduct() {}
+async function createNewProduct(req, res, next) {
+    const product = new Product({
+        ...req.body,
+        image: req.file.filename
+    });
+
+    try{
+        await product.save();
+
+    }catch (error){
+        next(error);
+        return;
+    }
+
+    res.redirect('/admin/products'); 
+}
 
 module.exports = {
     getProducts: getProducts,
